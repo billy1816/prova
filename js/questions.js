60964fc39444ece2adc53cdcc3b4f68dd21a2b26
billy1816 <billy1816_@hotmail.com>
2017-02-24 10:04:15 +0100

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -105,20 +105,5 @@ function ponerDatosSelectHtml(t,opt){
 
 //****************************************************************************************************
 //Gestionar la presentación de las respuestas
-function darRespuestaHtml(r){
- var p = document.createElement("p");
- var node = document.createTextNode(r);
- p.appendChild(node);
- document.getElementById('resultadosDiv').appendChild(p);
-}
-
-function presentarNota(){
-   darRespuestaHtml("Nota: "+nota+" puntos sobre 3");
-}
-
-function inicializar(){
-   document.getElementById('resultadosDiv').innerHTML = "";
-   nota=0.0;
-}
 
 
